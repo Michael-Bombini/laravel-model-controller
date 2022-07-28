79739aa22c4113dbd28aa133b82d457d57bc547c
Michael Bombini <bombini.michael@gmail.com>
2022-07-28 15:05:05 +0200

    Init

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    function index(){
+        $data = Movie::all();
+
+        return view('/', [
+            "movies" => $data
+        ]);
+        
+    }
+}
